81d46e871d1e5cfdf59f9942b12b02180ef97b8e
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-29 14:56:43 +0300

    Feat schema for genres

---
+++ models/genre.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+	name: { type: String, required: true, min_length: 3, max_length: 100 },
+
+	virtuals: {
+		url: {
+			get() {
+				return `/catalog/genre/${this._id}`;
+			},
+		},
+	},
+});
+
+module.exports = mongoose.model("Genre", GenreSchema);
