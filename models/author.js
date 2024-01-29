17e4bf3b5acb9b84345818400c86344e2ed83d44
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-29 15:06:40 +0300

    Fix virtuals being declared at the wrong place

--- models/author.js
+++ models/author.js
@@ -2,30 +2,33 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const AuthorSchema = new Schema({
-	first_name: { type: String, required: true, max_length: 100 },
-	family_name: { type: String, required: true, max_length: 100 },
-	date_of_birth: { type: Date },
-	date_of_death: { type: Date },
-
-	virtuals: {
-		name: {
-			get() {
-				let fullname = "";
-
-				if (this.first_name && this.family_name) {
-					fullname = first_name + " " + family_name;
-				}
-
-				return fullname;
-			},
-		},
-		url: {
-			get() {
-				return `/catalog/authors/${this._id}`;
-			},
-		},
+const AuthorSchema = new Schema(
+	{
+		first_name: { type: String, required: true, max_length: 100 },
+		family_name: { type: String, required: true, max_length: 100 },
+		date_of_birth: { type: Date },
+		date_of_death: { type: Date },
 	},
-});
+	{
+		virtuals: {
+			name: {
+				get() {
+					let fullname = "";
+
+					if (this.first_name && this.family_name) {
+						fullname = first_name + " " + family_name;
+					}
+
+					return fullname;
+				},
+			},
+			url: {
+				get() {
+					return `/catalog/authors/${this._id}`;
+				},
+			},
+		},
+	}
+);
 
 module.exports = mongoose.model("Author", AuthorSchema);
