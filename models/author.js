639c85c4b91e153ec9c6405773f50949010a7d44
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-29 14:39:55 +0300

    Feat schema for authors

---
+++ models/author.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+	first_name: { type: String, required: true, max_length: 100 },
+	family_name: { type: String, required: true, max_length: 100 },
+	date_of_birth: { type: Date },
+	date_of_death: { type: Date },
+
+	virtuals: {
+		name: {
+			get() {
+				let fullname = "";
+
+				if (this.first_name && this.family_name) {
+					fullname = first_name + " " + family_name;
+				}
+
+				return fullname;
+			},
+		},
+		url: {
+			get() {
+				return `/catalog/authors/${this._id}`;
+			},
+		},
+	},
+});
+
+module.exports = AuthorSchema;
