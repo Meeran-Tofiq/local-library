c438fa9593ed5e9fa7154c5d71e510e096d27e6b
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-01-29 14:24:31 +0300

    Feat DB connection

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const connectionString = require("./util/credentials");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+
+main().catch((err) => console.log(err));
+async function main() {
+	await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
