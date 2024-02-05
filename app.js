8638e97856bf44697ba4d8087b2b50825bdef87d
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-05 14:58:00 +0300

    Feat route handler for /catalog

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var mongoose = require("mongoose");
 const connectionString = require("./util/credentials");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var catalogRouter = require("./routes/catalog");
 
 var app = express();
 
 main().catch((err) => console.log(err));
 async function main() {
-	await mongoose.connect(mongoDB);
+	// await mongoose.connect(mongoDB);
 }
 
 // view engine setup
@@ -26,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
