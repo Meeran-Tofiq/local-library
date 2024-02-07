a36ef4be208b82da66f45cfffadf336062ad2e69
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-07 15:03:22 +0300

    Fix referencing first name without this keyword

--- models/author.js
+++ models/author.js
@@ -16,7 +16,7 @@ const AuthorSchema = new Schema(
 					let fullname = "";
 
 					if (this.first_name && this.family_name) {
-						fullname = first_name + " " + family_name;
+						fullname = this.family_name + ", " + this.first_name;
 					}
 
 					return fullname;
