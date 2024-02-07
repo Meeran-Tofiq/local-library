cd025252b3a52b0091720b10ee15e1504fb44c07
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-07 23:11:30 +0300

    Feat post handling for genre creation

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -36,9 +36,37 @@ exports.genre_create_get = (req, res, next) => {
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: Genre create POST");
-});
+exports.genre_create_post = [
+	body("name", "Genre name must contain at least 3 characters")
+		.trim()
+		.isLength({ min: 3 })
+		.escape(),
+	asyncHandler(async (req, res, next) => {
+		const errors = validationResult(req);
+		const genre = new Genre({ name: req.body.name });
+
+		if (!errors.isEmpty()) {
+			res.render("genre_form", {
+				title: "Create Genre",
+				genre: genre,
+				errors: errors.array(),
+			});
+			return;
+		}
+
+		const genreExists = await Genre.findOne({ name: req.body.name })
+			.collation({ locale: "en", strength: 2 })
+			.exec();
+
+		if (genreExists) {
+			res.redirect(genreExists.url);
+			return;
+		}
+
+		await genre.save();
+		res.redirect(genre.url);
+	}),
+];
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
