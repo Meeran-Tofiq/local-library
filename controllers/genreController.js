f23a34d42e7ab1d0699eb5132d27a02bbac9695b
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-07 22:31:32 +0300

    Feat route handling for genre create page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,5 +1,6 @@
 const Genre = require("../models/genre");
 const Book = require("../models/book");
+const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Genre.
@@ -30,9 +31,9 @@ exports.genre_detail = asyncHandler(async (req, res, next) => {
 });
 
 // Display Genre create form on GET.
-exports.genre_create_get = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: Genre create GET");
-});
+exports.genre_create_get = (req, res, next) => {
+	res.render("genre_form", { title: "Create Genre" });
+};
 
 // Handle Genre create on POST.
 exports.genre_create_post = asyncHandler(async (req, res, next) => {
