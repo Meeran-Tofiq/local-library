c65394f0e7f95629717c8bffb26eb7e86ba83c18
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-07 20:11:15 +0300

    Fix error being written when a date wasn't given

--- models/author.js
+++ models/author.js
@@ -28,16 +28,30 @@ const AuthorSchema = new Schema(
 					return `/catalog/authors/${this._id}`;
 				},
 			},
+			date_of_birth_formatted: {
+				get() {
+					return this.date_of_birth
+						? DateTime.fromJSDate(
+								this.date_of_birth
+						  ).toLocaleString(DateTime.DATE_MED)
+						: " ";
+				},
+			},
+			date_of_death_formatted: {
+				get() {
+					return this.date_of_death
+						? DateTime.fromJSDate(
+								this.date_of_death
+						  ).toLocaleString(DateTime.DATE_MED)
+						: " ";
+				},
+			},
 			lifespan: {
 				get() {
 					return (
-						DateTime.fromJSDate(this.date_of_birth).toLocaleString(
-							DateTime.DATE_MED
-						) +
+						this.date_of_birth_formatted +
 						" - " +
-						DateTime.fromJSDate(this.date_of_death).toLocaleString(
-							DateTime.DATE_MED
-						)
+						this.date_of_death_formatted
 					);
 				},
 			},
