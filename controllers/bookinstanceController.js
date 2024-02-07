215d9a352753be9bd31449fd8de1c4562c93ee8f
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-07 15:19:33 +0300

    Feat route handling for book instance list

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -3,7 +3,14 @@ const asyncHandler = require("express-async-handler");
 
 // Display list of all BookInstances.
 exports.bookinstance_list = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: BookInstance list");
+	const allBookInstances = await BookInstance.find({})
+		.populate("book")
+		.exec();
+
+	res.render("bookinstance_list", {
+		title: "Book Copies",
+		all_book_instances: allBookInstances,
+	});
 });
 
 // Display detail page for a specific BookInstance.
