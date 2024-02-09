7d11f5c9116bef5e0ef959f2cffa4de154fc63c5
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-02-09 13:28:57 +0300

    Feat route handling for author update page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -134,7 +134,7 @@ exports.author_delete_post = asyncHandler(async (req, res, next) => {
 
 	if (allBooksByAuthor.length > 0) {
 		res.render("author_delete", {
-			title: "Delete Author",
+			title: "Delete author",
 			author: author,
 			author_books: allBooksByAuthor,
 		});
@@ -147,7 +147,12 @@ exports.author_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Author update form on GET.
 exports.author_update_get = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: Author update GET");
+	const author = await Author.findById(req.params.id).exec();
+
+	res.render("author_form", {
+		title: "Update author",
+		author,
+	});
 });
 
 // Handle Author update on POST.
